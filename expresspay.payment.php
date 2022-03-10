2ff2fd869ecb7cfe0245bbe22175af5e7ad7f88a
dmitriy.tsvetkov <dmitriy.tsvetkov@hb.by>
2022-03-10 12:48:39 +0300

    Version: 1.1.6

--- expresspay.payment.php
+++ expresspay.payment.php
@@ -3,7 +3,7 @@
 Plugin Name: ExpressPay Payment Module
 Plugin URI: https://express-pay.by/cms-extensions/wordpress
 Description: Place the plugin shortcode at any of your pages and start to accept payments in WordPress instantly
-Version: 1.1.5
+Version: 1.1.6
 Author: LLC «TriIncom»
 Author URI: https://express-pay.by
 Text Domain: wordpress_expresspay
